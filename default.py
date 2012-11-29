7eb062fc9f087c9932b4c74dd59ab53e9b891074
takiaine <anttikolu@gmail.com>
2012-11-29 18:58:06 +0200

    first working implementation of the addon(again)

--- default.py
+++ default.py
@@ -1,4 +1,18 @@
-import xbmc;
- 
+import xbmc
+import subprocess
+
 while (not xbmc.abortRequested):
-  #Some code
+	print "check new values"
+	retVal = subprocess.check_output(["bash", "/home/antti/.xbmc/addons/MythTVPowersaveAddon/checkMythStatus.sh"])
+	print retVal
+	try:
+		ret = int(retVal)
+	except ValueError:
+		ret = 1
+	print ret
+	it = xbmc.getGlobalIdleTime()
+	print it
+	if(it > 60*60 and ret == 0 and xbmc.getCondVisibility("System.ScreenSaverActive")):
+		xbmc.executebuiltin('XBMC.Quit()')
+		print "executebuiltin('XBMC.Quit()')"
+	xbmc.sleep(60000)
